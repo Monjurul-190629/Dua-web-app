fac227961dcc8489a0d7cb91f9c7a52eecb6fd94
Monjurul Alam <monjurulalam190629@gmail.com>
2025-04-14 17:13:46 +0600

    subcategories and duas is added now

---
+++ backend/api/subcategories.js
@@ -0,0 +1,12 @@
+import sqlite3 from 'sqlite3';
+export default function handler(req, res) {
+  const db = new sqlite3.Database('dua_main.sqlite');
+  db.all('SELECT * FROM sub_category', [], (err, rows) => {
+    if (err) {
+        res.status(500).json({ error: err.message });
+    }
+    else{
+        res.status(200).json(rows);
+    }
+  });
+}
