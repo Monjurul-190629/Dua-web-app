31ce9d1af0aa4d4b72d4d0a60f8b56f763ce8db6
Monjurul Alam <monjurulalam190629@gmail.com>
2025-04-14 14:50:49 +0600

    successfully connect to dua_main database

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const sqlite3 = require('sqlite3').verbose();
 require('dotenv').config();
 
 
@@ -11,6 +12,19 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+
+
+// Connect to SQLite database
+const db = new sqlite3.Database('./dua_main.sqlite', (err) => {
+    if (err) {
+        console.error('Error opening database:', err.message);
+    } else {
+        console.log('Connected to the SQLite database.');
+    }
+});
+
+
+
 
 
 /// default
